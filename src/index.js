41494df68e6bcfa75233efe7daafecda7a5246a1
MelanieBerrut <melanieberrut.dev@gmail.com>
2017-03-26 16:13:30 +0100

    [feature] Converted a stateless component into a statefull

--- src/index.js
+++ src/index.js
@@ -10,15 +10,19 @@ const Book = ({ book }) => (
 	</li>
 )
 
-const Booklist = (props) => (
-	<ul>
-		{props.books.map(book =>{
-			return (
-				<Book key={ book.id } book={ book } />
-			);
-		})}
-	</ul>
-)
+class Booklist extends React.Component {
+	render(){
+		return (
+			<ul>
+				{this.props.books.map(book =>{
+					return (
+						<Book key={ book.id } book={ book } />
+					);
+				})}
+			</ul>
+		);
+	}
+}
 
 ReactDOM.render(
   <Booklist books={ data.books } />,
